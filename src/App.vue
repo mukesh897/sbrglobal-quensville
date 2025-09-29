9491e0bbc8e5caefbf4f09345cc75cae8632782f
mukesh897 <44204655+mukesh897@users.noreply.github.com>
2025-09-29 13:17:59 +0530

    final disclaimer page added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-white">
+  <div class="min-h-screen bg-white" v-if="$route.path === '/'">
     <!-- Navigation -->
     <nav class="bg-white shadow-sm border-b border-gray-200 sticky top-0 z-50">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -862,6 +862,7 @@
       </div>
     </div>
   </div>
+  <router-view v-else />
 </template>
 
 <script>
